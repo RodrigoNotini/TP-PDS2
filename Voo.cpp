c049df3c0275c505e609da530b8c019e905862d9
RodrigoNotini <rodrigo.ferr.not@gmail.com>
2024-06-20 01:14:09 -0300

    Update Voo.cpp

--- Voo.cpp
+++ Voo.cpp
@@ -1,7 +1,10 @@
 #include "Voo.hpp"
 using namespace std;
-Voo::Voo(const string& _origem,const string& _destino,int _num_assentos,vector<bool>_assentos)
-        :origem(_origem),destino(_destino),num_assentos(_num_assentos),assentos(_assentos){}
+Voo::Voo(const string& _origem,const string& _destino,int _num_assentos,vector<bool>_assentos,int _preço)
+        :origem(_origem),destino(_destino),num_assentos(_num_assentos),assentos(_assentos),preço(_preço){}
+int Voo::GetPreço(){
+    return preço;
+}
 bool Voo::Checar_assentos() {
         auto it= assentos.begin();
         while (it != assentos.end()) {
