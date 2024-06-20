e748627212b41aa822edefbcc21bb5564b448686
RodrigoNotini <rodrigo.ferr.not@gmail.com>
2024-06-20 01:13:37 -0300

    Update Usuario.cpp

--- Usuario.cpp
+++ Usuario.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include "usuario.hpp"
 using namespace std;
-Usuario::Usuario(const string& _nome, const string& _gmail, int _id, int _senha)
-     :nome(_nome), gmail(_gmail), id(_id), senha(_senha){}
+Usuario::Usuario(const string& _nome, const string& _gmail, int _id, int _senha,int _saldo)
+     :nome(_nome), gmail(_gmail), id(_id), senha(_senha), saldo(_saldo){}
 string Usuario::GetNome(){
     return nome;
 }
-
-
-
+int Usuario::GetSaldo(){
+    return saldo;
+}
 void Usuario::Trocar_Senha(int senha_atual, int nova_senha) {
     if (senha_atual == senha) {
     senha = nova_senha;
