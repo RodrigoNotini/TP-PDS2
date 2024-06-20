392fbbf931560743540c14591d855459ecd42008
RodrigoNotini <rodrigo.ferr.not@gmail.com>
2024-06-20 01:12:39 -0300

    Update Usuario.hpp

--- Usuario.hpp
+++ Usuario.hpp
@@ -11,9 +11,10 @@ class Usuario{
     int id;
     int saldo;
     public:
-    Usuario(const string& _nome, const string& _gmail, int _id, int _senha);//construtor
+    Usuario(const string& _nome, const string& _gmail, int _id, int _senha,int _saldo);//construtor
     string GetNome();
+    int GetSaldo();
     void Trocar_Senha(int senha_atual, int nova_senha);
     void Trocar_Nome(string novo_nome);//nao precisa saber o nome antigo se quer mudar nome
-    
+
 };
