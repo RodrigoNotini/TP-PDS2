6447f40e63c86b050f3e2a2f507c641b8377c999
RodrigoNotini <rodrigo.ferr.not@gmail.com>
2024-06-19 21:53:51 -0300

    Create Gerenciador_sistema.hpp

---
+++ Gerenciador_sistema.hpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include<iostream>
+#include"usuario.hpp"
+class Gerenciador_Sistema{
+    private:
+    vector<Usuario>Usuarios;
+    public:
+    void remover_usuario(string nome_remov);
+    
+};
