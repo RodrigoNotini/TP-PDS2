e16cd2413e5746568ee6996eed15f152b06fa901
RodrigoNotini <rodrigo.ferr.not@gmail.com>
2024-06-19 21:53:24 -0300

    Create Voo.cpp

---
+++ Voo.cpp
@@ -0,0 +1,14 @@
+#include "Voo.hpp"
+using namespace std;
+Voo::Voo(const string& _origem,const string& _destino,int _num_assentos,vector<bool>_assentos)
+        :origem(_origem),destino(_destino),num_assentos(_num_assentos),assentos(_assentos){}
+bool Voo::Checar_assentos() {
+        auto it= assentos.begin();
+        while (it != assentos.end()) {
+            if (*it != 1) {
+                return false;  // Retorna falso se encontrar um assento não ocupado
+            }
+            it++;  // Avança para o próximo assento
+        }
+        return true;  // Retorna verdadeiro se todos os assentos estiverem ocupados
+    };
