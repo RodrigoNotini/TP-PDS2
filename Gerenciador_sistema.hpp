65bdef1e87c626668d120c57d2db01541c87210f
RodrigoNotini <rodrigo.ferr.not@gmail.com>
2024-06-20 01:14:57 -0300

    Update Gerenciador_sistema.hpp

--- Gerenciador_sistema.hpp
+++ Gerenciador_sistema.hpp
@@ -1,10 +1,13 @@
 #include <vector>
 #include<iostream>
-#include"usuario.hpp"
+#include "Usuario.hpp"
+#include "Pagamento.hpp"
 class Gerenciador_Sistema{
     private:
     vector<Usuario>Usuarios;
+    vector<Pagamento>Pagamentos;
     public:
     void remover_usuario(string nome_remov);
+    void remover_pagamento(int _id); //apaga pagamentos aprovados do vetor para nao ocupar tanta memória
     
 };
