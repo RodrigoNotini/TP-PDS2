111a1bb95cd804809113d0b02864a6a838ce7ccf
RodrigoNotini <rodrigo.ferr.not@gmail.com>
2024-06-19 21:52:12 -0300

    Create Usuario.hpp

---
+++ Usuario.hpp
@@ -0,0 +1,19 @@
+#include <string>
+using namespace std;
+
+
+
+class Usuario{
+    private:
+    string nome;
+    string gmail;
+    int senha;
+    int id;
+    int saldo;
+    public:
+    Usuario(const string& _nome, const string& _gmail, int _id, int _senha);//construtor
+    string GetNome();
+    void Trocar_Senha(int senha_atual, int nova_senha);
+    void Trocar_Nome(string novo_nome);//nao precisa saber o nome antigo se quer mudar nome
+    
+};
