d6c5fd282506087cc33f6b71167837b2d500e42e
RodrigoNotini <rodrigo.ferr.not@gmail.com>
2024-06-21 01:25:17 -0300

    Update Pagamento.hpp

--- Pagamento.hpp
+++ Pagamento.hpp
@@ -7,9 +7,11 @@ class Pagamento{
     bool sit_pagamento;
     string forma_pagamento;
     int id_pagamento;
+    int preço_pagamento;
     public:
-    Pagamento(bool _sit_pagamento,const string& _forma_pagamento,int _id_pagamento);
+    Pagamento(Gerenciador_Sistema sistema,bool _sit_pagamento,const string& _forma_pagamento,int _id_pagamento,int _preço_pagamento);
+    int GetPreço_Pagamento();
     int GetId ();
     void Comprar_Passagem(Usuario* cliente,Voo* voo_desejado,string _forma_pagamento);
-    void Reembolso(Usuario* cliente,int _id_pagamento,string _forma_pagamento);
+    string Reembolso(Usuario* cliente,Gerenciador_Sistema* sistema,int _id_pagamento,string _forma_pagamento);
 };
