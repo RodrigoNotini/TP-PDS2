73b8ef4a7245b294cd69d22bda1f7a1196178d79
RodrigoNotini <rodrigo.ferr.not@gmail.com>
2024-06-21 01:27:18 -0300

    Update Voo.hpp

--- Voo.hpp
+++ Voo.hpp
@@ -1,15 +1,18 @@
 #include <string>
 #include <vector>
+#include "Gerenciador_sistema.hpp"
 using namespace std;
 class Voo{
     private:
     string origem;
     string destino;
+    int horas;
+    int minutos;
     int num_assentos;
     int preço;
     vector<bool>assentos;
     public:
-    Voo(const string& _origem,const string& _destino,int _num_assentos,vector<bool>_assentos,int _preço);
+    Voo(Gerenciador_Sistema* sistema,const string& _origem,const string& _destino,int _horas,int _minutos,int _num_assentos,vector<bool>_assentos,int _preço);
     int GetPreço();
     bool Checar_assentos();
 
