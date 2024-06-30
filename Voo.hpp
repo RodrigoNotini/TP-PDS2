b774ccd1e47d1876529109963c6ba4d7021f2107
RodrigoNotini <rodrigo.ferr.not@gmail.com>
2024-06-30 18:18:19 -0300

    Update Voo.hpp

--- Voo.hpp
+++ Voo.hpp
@@ -1,20 +1,28 @@
+
+#ifndef VOO_HPP
+#define VOO_HPP
+
 #include <string>
 #include <vector>
-//problemas com nome//função nao chamando estao relacionados a problemas de include
 using namespace std;
 class Voo{
-    private:
+private:
     string origem;
     string destino;
     int horas;
     int minutos;
     int num_assentos;
-    int preço;
-    vector<bool>assentos;
-    public:
-    Voo(const string& _origem,const string& _destino,int _horas,int _minutos,int _num_assentos,vector<bool>_assentos,int _preço);
+    int preco;
+    int id_voo;
+    vector<bool> assentos;
+
+public:
+    Voo(const string& _origem, const string& _destino, int _horas, int _minutos, int _num_assentos, vector<bool> _assentos, int _preco,int id_voo);
     int GetHoras();
     int GetMinutos();
-    int GetPreço();
+    int GetPreco();
+    int GetId_Voo();
     bool Checar_assentos();
 };
+
+#endif
