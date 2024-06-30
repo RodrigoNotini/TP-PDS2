1deb68707ba6c30d2a70db59b52bea732b343e63
RodrigoNotini <rodrigo.ferr.not@gmail.com>
2024-06-30 18:15:30 -0300

    Update Gerenciador_sistema.hpp

--- Gerenciador_sistema.hpp
+++ Gerenciador_sistema.hpp
@@ -1,19 +1,31 @@
+#ifndef GERENCIADOR_SISTEMA_HPP
+#define GERENCIADOR_SISTEMA_HPP
+
 #include <vector>
-#include<iostream>
+#include <iostream>
 #include "Pagamento.hpp"
 #include "Voo.hpp"
 #include "Usuario.hpp"
-class Gerenciador_Sistema{
-    private:
-    vector<Usuario>Usuarios;
-    vector<Pagamento>Pagamentos;
-    vector<Voo>Voos;
-    public:
+
+using namespace std;
+
+class Gerenciador_Sistema {
+private:
+    vector<Usuario> Usuarios;
+    vector<Pagamento> Pagamentos;
+    vector<Voo> Voos;
+
+public:
     vector<Usuario>& GetUsuarios();
     void adicionar_usuario(Usuario& usuario);
     void remover_usuario(string nome_remov);
-    void remover_pagamento(int _id); //apaga pagamentos aprovados do vetor para nao ocupar tanta memória.
+    void remover_pagamento(int _id); // apaga pagamentos aprovados do vetor para nao ocupar tanta memória.
     vector<Pagamento>* GetPagamentos();
-    string MostrarHorários();//mostra os horários de todos os voos.
-    
+    string MostrarHorarios();// mostra os horários de todos os voos.
+    void MostrarIdVoos(); 
+    bool ConfereReembolso(Pagamento* pagamento);
+    void adicionar_voo(Voo& voo);
+    void criarVoosDeTeste();
 };
+
+#endif
