13a86b6efe77dffeafaff4b9c94f02768d4f3685
Eduardo Junio <junioeduardo30@gmail.com>
2024-07-02 14:14:47 -0300

    Update Avaliacao.hpp

--- Avaliacao.hpp
+++ Avaliacao.hpp
@@ -1,5 +1,9 @@
+#pragma once
 #include <string>
+#include <iostream>
+
 using namespace std;
+
 class Avaliacao {
 private:
     int idUsuario;
@@ -7,9 +11,47 @@ private:
     int nota;
 
 public:
-    Avaliacao(int _idUsuario, const string& _comentario, int _nota);
+    // Construtor com validações
+    Avaliacao(int _idUsuario, const string& _comentario, int _nota) 
+        : idUsuario(_idUsuario), comentario(_comentario), nota(_nota) {
+        if (_nota < 0 || _nota > 5) {
+            throw invalid_argument("Nota deve estar entre 0 e 5");
+        }
+    }
 
-    int GetId() const;
-    string GetComentario() const;
-    int GetNota() const;
+    // Métodos 'get'
+    int GetId() const {
+        return idUsuario;
+    }
+
+    string GetComentario() const {
+        return comentario;
+    }
+
+    int GetNota() const {
+        return nota;
+    }
+
+    // Métodos 'set' com validações
+    void SetId(int _idUsuario) {
+        idUsuario = _idUsuario;
+    }
+
+    void SetComentario(const string& _comentario) {
+        comentario = _comentario;
+    }
+
+    void SetNota(int _nota) {
+        if (_nota < 0 || _nota > 5) {
+            throw invalid_argument("Nota deve estar entre 0 e 5");
+        }
+        nota = _nota;
+    }
+
+    // Método para exibir os dados da avaliação
+    void ExibirAvaliacao() const {
+        cout << "ID do Usuário: " << idUsuario << endl;
+        cout << "Comentário: " << comentario << endl;
+        cout << "Nota: " << nota << endl;
+    }
 };
