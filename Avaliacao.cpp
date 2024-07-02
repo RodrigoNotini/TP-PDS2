25405df78be6cd0aa4907f990e5fa0ebefdd986f
Eduardo Junio <junioeduardo30@gmail.com>
2024-07-02 14:08:43 -0300

    Update Avaliacao.cpp

--- Avaliacao.cpp
+++ Avaliacao.cpp
@@ -1,16 +1,38 @@
+#include <string>
+#include <stdexcept> // Incluindo o cabeçalho para exceções padrão como std::invalid_argument
 #include "Avaliacao.hpp"
-using namespace std;
-Avaliacao::Avaliacao(int _idUsuario, const std::string& _comentario, int _nota)
-    : idUsuario(_idUsuario), comentario(_comentario), nota(_nota) {}
 
-int Avaliacao::GetId() const {
+/**
+ * @brief Classe que representa uma avaliação de um usuário para um sistema de reserva de passagens aéreas.
+ */
+Avaliacao::Avaliacao(int _idUsuario, const std::string& _comentario, int _nota)
+    : idUsuario(_idUsuario), comentario(_comentario) {
+    if (_nota < 0 || _nota > 10) {
+        throw std::invalid_argument("Nota deve estar entre 0 e 10.");
+    }
+    nota = _nota;
+}
+
+/**
+ * @brief Obtém o ID do usuário que fez a avaliação.
+ * @return ID do usuário.
+ */
+int Avaliacao::GetId() const noexcept {
     return idUsuario;
 }
 
-string Avaliacao::GetComentario() const {
+/**
+ * @brief Obtém o comentário da avaliação.
+ * @return Comentário da avaliação.
+ */
+std::string Avaliacao::GetComentario() const {
     return comentario;
 }
 
-int Avaliacao::GetNota() const {
+/**
+ * @brief Obtém a nota da avaliação.
+ * @return Nota da avaliação.
+ */
+int Avaliacao::GetNota() const noexcept {
     return nota;
 }
